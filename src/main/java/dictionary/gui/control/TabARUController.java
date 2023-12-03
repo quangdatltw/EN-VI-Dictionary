62f48a476423ff0b62935eb8ec2dcc9e9707a8f6
quangdatltw <quangdatltw@gmail.com>
2023-12-04 00:03:05 +0700

    Finished addExample method for TabARU
    - 10% unfinished:
    + undo, redo

--- src/main/java/dictionary/gui/control/TabARUController.java
+++ src/main/java/dictionary/gui/control/TabARUController.java
@@ -19,17 +19,21 @@ public class TabARUController {
         reset();
         searchWord.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!oldValue.equals(newValue)) {
-                addWord.setDisable(true);
-                updateWord.setDisable(true);
-                removeWord.setDisable(true);
-                meaning.setDisable(true);
-                example.setDisable(true);
-                wordType.setDisable(true);
+                reset();
             }
             assert newValue != null;
             searchWord.setText(newValue.toLowerCase());
             checkWord();
         });
+
+        wordType.valueProperty().addListener((observable, oldValue, newValue) -> {
+            wordMeanings.setValue("");
+            meaningText.setText("");
+            exampleText.setText("");
+            example.setDisable(true);
+        });
+
+
 
     }
 
@@ -38,14 +42,18 @@ public class TabARUController {
     }
 
     private void reset() {
-        searchWord.setText("");
         wordDef = "";
+        exampleText.setText("");
+        meaningText.setText("");
+        wordMeanings.setValue("");
+        wordType.setValue("");
         addWord.setDisable(true);
         updateWord.setDisable(true);
         removeWord.setDisable(true);
         meaning.setDisable(true);
         example.setDisable(true);
         wordType.setDisable(true);
+        wordMeanings.setDisable(true);
     }
 
     private void concatDef(String add) {
@@ -131,10 +139,9 @@ public class TabARUController {
         String type = wordType.getValue();
         if (type != null && !wordDef.contains(type)) {
             concatDef(type);
-
-            meaning.setDisable(false);
-            example.setDisable(false);
         }
+        meaning.setDisable(false);
+        wordMeanings.setDisable(false);
     }
 
     private String cutWordType() {
@@ -142,17 +149,18 @@ public class TabARUController {
 
         if (str.contains("\n*")) {
             str = str.substring(0, str.indexOf("\n*"));
+            str = str.substring(0, str.lastIndexOf("\n"));
         }
 
         if (str.contains(wordTypesList.get(6))) {
             str = str.substring(0, str.indexOf(wordTypesList.get(6)));
         }
-        System.out.println(str);
+
         return  str;
     }
     @FXML
     public void getWordMeaningList() {
-        String str = cutWordType();
+        String str = cutWordType() + "\n";
 
         List<String> meaningList = new ArrayList<>();
         while (str.contains("-")) {
@@ -165,22 +173,29 @@ public class TabARUController {
 
         wordMeanings.setItems(FXCollections.observableArrayList(meaningList));
     }
+    @FXML
+    public void setWordMeaning() {
+        example.setDisable(false);
+    }
 
     @FXML
     public void addMeaning() {
-        if (wordDef.contains(meaningText.getText())) {
+        if (wordDef.contains("- " + meaningText.getText().trim().replace("\n", ""))) {
             return;
         }
         String set = cutWordType();
-        System.out.println(set);
-        wordDef = wordDef.replace(set, set + "\n    - " + meaningText.getText());
-        System.out.println(wordDef);
+        wordDef = wordDef.replace(set, set + "\n    - " + meaningText.getText().replace("\n", ""));
         prototypeText.setText(wordDef);
     }
 
     @FXML
     public void addExample() {
-
+        if (wordDef.contains("VD: " + exampleText.getText().trim().replace("\n", ""))) {
+            return;
+        }
+        String mean = wordMeanings.getValue();
+        wordDef = wordDef.replace(mean, mean + "\n       VD: " + exampleText.getText());
+        prototypeText.setText(wordDef);
     }
 
     @FXML
